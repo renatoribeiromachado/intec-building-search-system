5075c2d35ea27988726a76237a986e10bb1b4546
Raphael Paulino <raphael.paulino.web@gmail.com>
2023-07-22 22:37:26 -0300

    feat: add missing route

--- resources/views/layouts/app_customer.blade.php
+++ resources/views/layouts/app_customer.blade.php
@@ -26,7 +26,7 @@
     <body>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">INTEC</a>
+                <a class="navbar-brand" href="{{ route('dashboard') }}">INTEC</a>
                 <button
                     class="navbar-toggler"
                     type="button"
