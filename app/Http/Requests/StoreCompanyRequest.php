27ae8f4fc2337dd7840829181512953690e5bbe9
Renato Ribeiro Machado <renato@acessohost.com.br>
2023-07-25 15:48:24 -0300

    feat: Incluido regras de validação de empresas

---
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = $this->segment(3);
+        return [
+
+            'company_name' => "required|min:3|max:150|unique:companies,company_name,{$id},id",
+            'trading_name' => "required",
+            'revision' => "required",
+            'cnpj' => "required",
+            'primary_email' => "required",
+            'phone_one' => "required",
+            'zip_code' => "required",
+            'address' => "required",
+            'number' => "required",
+            'complement' => "required",
+            'district' => "required",
+            'city' => "required",
+            'state' => "required",
+            'state_registration' => "required",
+            'city_registration' => "required",
+            'activity_field_id' => "required",
+            'is_active' => "required",
+            'home_page' => "required",
+            'email' => "required",
+            'last_review' => "required",
+            'researcher_id' => "required",
+            'notes' => "required"
+        ];
+    }
+}
