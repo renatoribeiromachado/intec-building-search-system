f05fdbbcaf8d3ce6ebe56c494842537fc7128b7c
Renato Ribeiro Machado <renato@acessohost.com.br>
2023-07-25 13:32:21 -0300

    refactor: reformulação do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,58 +1,89 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-    <head>
-        {{-- <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title> --}}
+@extends('layouts.app')
+@section('content')
 
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<section class="h-100 gradient-form" style="background-color: #eee;">
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-xl-10">
+                <div class="card rounded-3 text-black shadow">
+                    <div class="row g-0">
+                        <div class="col-lg-6">
+                            <div class="card-body p-md-5 mx-md-4">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                                <div class="text-center">
+                                    <img src='{{ url('images/logomarca.png') }}' style="width: 285px;"/>
+                                    <h4 class="mt-1 mb-5 pb-1">Pesquisa de Obras</h4>
+                                </div>
+                                <!--Mensagem que vem da classe app/Http/Middleware/SingleDeviceSessionMiddleware, qdo o mesmo usuario com o mesmo se login se loga em outro dispositivo-->
+                                <div class="text-center">
+                                    @if(session('message'))
+                                        <div class="alert alert-danger">
+                                            {{ session('message') }}
+                                        </div>
+                                    @endif
+                                </div>
 
-        <title>SINTEC</title>
-    </head>
-    <body>
-        <div class="container col-xl-10 col-xxl-8 px-4 py-5">
-            <div class="row align-items-center g-lg-5 py-5">
-            <div class="col-lg-7 text-center text-lg-start">
-                <h1 class="display-4 fw-bold lh-1 mb-3">Bem-vindos a SINTEC</h1>
-                <p class="col-lg-10 fs-4">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
-            </div>
-            <div class="col-md-10 mx-auto col-lg-5">
-                <form method="POST" action="{{ route('login') }}" class="p-4 p-md-5 border rounded-3 bg-light">
-                    @csrf
-                        
-                    <div class="form-floating mb-3">
-                        <input type="email" name="email" class="form-control" id="floatingInput" value="{{ old('email') }}" placeholder="name@example.com">
-                        <label for="floatingInput">Email</label>
-                        @error('email') <span class="text-danger">{{ $errors->first('email') }}</span> @enderror
+                                <form method="POST" action="{{ route('login') }}" id="myForm">
+                                    @csrf  
+
+                                    <div class="form-outline mb-4">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" 
+                                               name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
+                                               placeholder="Digite seu e-mail">
+
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <div class="input-group">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" 
+                                                   name="password" required autocomplete="current-password"
+                                                   placeholder="Informe a senha">
+                                            <button id="togglePassword" class="btn btn-outline-secondary" type="button" onclick="togglePasswordVisibility()">
+                                                <i id="toggleIcon" class="bi bi-eye"></i>
+                                            </button>
+                                        </div>
+
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="text-center pt-1 mb-5 pb-1">
+                                        <button id="submitButton" type="submit" class="btn btn-primary btnSubmit">
+                                            {{ __('Avançar') }}
+                                        </button>
+                                        @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            <strong>{{ __('Esqueceu sua senha?') }}</strong>
+                                        </a>
+                                        @endif
+                                    </div>
+                                </form>
+                                <div class="col-md-12">
+                                    <p class="small"><a href="https://www.intecbrasil.com.br/site/politicadeprivacidade.html" target="_blank">Política de privacidade</a></p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-6 d-flex align-items-center gradient-custom-2" style="background-color: #ff3b00;">
+                            <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                <h4 class="mb-4">Intec Brasil</h4>
+                                <p class="small mb-0">O sistema INTEC de pesquisa de obras é o sistema totalmente online, para pesquisa de oportunidadesde negócios na construção civil de grande porte. A INTEC cataloga diariamente obras em projeto e construção, alimentando nossa base de dados com um verdadeiro Raio-X de cada uma, informando a construtora com os seus dados completos, estágio, prazos e previsões, empresas já contratadas, e contatos de responsáveis pela contratação da mão de obra ou compra de material.
+Nosso sistema de pesquisa de obras não limita informações por clicks, dando assim ao nosso assinante, total acesso aos leads que estejam dentro de seu plano.</p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-floating mb-3">
-                        <input type="password" name="password" value="" class="form-control" id="floatingPassword" required autocomplete="current-password" placeholder="{{ __('Password') }}">
-                        <label for="floatingPassword">Senha</label>
-                        @error('password') <span class="text-danger">{{ $errors->first('password') }}</span> @enderror
-                    </div>
-                    {{-- <div class="checkbox mb-3">
-                        <label>
-                            <input type="checkbox" value="remember-me">{{ __('Remember me') }}
-                        </label>
-                    </div> --}}
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
-                    <hr class="my-4">
-                    {{-- <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small> --}}
-                    <small class="text-muted">
-                        <a href="{{ route('register') }}" target="_blank">Não tem cadastro? Clique aqui.</a>
-                    </small>
-                </form>
-            </div>
+                </div>
             </div>
         </div>
+    </div>
 
-        <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
-</html>
+</section>
+@endsection
