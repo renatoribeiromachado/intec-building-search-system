57007c9d2618ba8b255db7ddd629f3aa881059f6
Raphael Paulino <raphael.paulino.web@gmail.com>
2023-07-28 19:04:34 -0300

    refactor: enable drop companies table

--- database/migrations/2023_07_22_212721_create_companies_table.php
+++ database/migrations/2023_07_22_212721_create_companies_table.php
@@ -81,6 +81,6 @@ class CreateCompaniesTable extends Migration
      */
     public function down()
     {
-        // Schema::dropIfExists('companies');
+        Schema::dropIfExists('companies');
     }
 }
