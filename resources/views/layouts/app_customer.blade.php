69dc205277fcd9e9f2c374f4119c8f56535d4cde
Renato Ribeiro Machado <renato@acessohost.com.br>
2023-08-08 09:00:37 -0300

    refactor: reafatorado codigo

--- resources/views/layouts/app_customer.blade.php
+++ resources/views/layouts/app_customer.blade.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Bootstrap CSS -->
+        <!-- Bootstrap5 CSS -->
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
