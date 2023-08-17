f32c7d94f721a6594aba80c350577a51b3b1b790
Raphael Paulino <raphael.paulino.web@gmail.com>
2023-08-17 09:41:06 -0300

    feat: finish company step 3 screen

--- app/Http/Controllers/CompanySearchController.php
+++ app/Http/Controllers/CompanySearchController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\ActivityField;
+use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 
 class CompanySearchController extends Controller
 {
     const REGISTRIES_PER_PAGE = 50;
 
     protected $activityField;
+    protected $company;
 
     public function __construct(
-        ActivityField $activityField
+        ActivityField $activityField,
+        Company $company
     ) {
         $this->activityField = $activityField;
+        $this->company = $company;
     }
 
     public function showCompanySearchStepOne()
@@ -26,8 +31,6 @@ class CompanySearchController extends Controller
             ->orderBy('description', 'asc')
             ->get();
 
-        // return $activityFields;
-
         return view('layouts.company.search.step_one.index', compact(
             'activityFields',
         ));
@@ -35,17 +38,25 @@ class CompanySearchController extends Controller
 
     public function showWorkSearchStepTwo(Request $request)
     {
-        // $activityFields = $this->activityField
-        //     ->select('id', 'description')
-        //     ->orderBy('description', 'asc')
-        //     ->get();
-
-        // return $activityFields;
-
         $companies = $this->getFilteredCompanies($request);
 
         return view('layouts.company.search.step_two.index', compact(
             'companies',
+        ));
+    }
+
+    public function showWorkSearchStepThree(Request $request)
+    {
+        // $this->authorize('ver-pesquisa-de-empresas');
+        
+        $companies = $this->getFilteredCompanies($request);
+        // $workFeatures = $this->workFeature
+        //     ->orderBy('description', 'asc')
+        //     ->get();
+
+        return view('layouts.company.search.step_three.index', compact(
+            'companies',
+            // 'workFeatures'
         ));
     }
 
@@ -54,10 +65,20 @@ class CompanySearchController extends Controller
         $loggedUser = Auth::user();
         $segments = $request->segments;
 
-        $companies = DB::table('companies');
+        $companies = $this->company;
+
+        $allCompanyIds = null;
+        if (/*(session()->has($this->companiesSessionName) || $request->companies_selected) &&*/
+            ! Route::is('company.search.step_two.index')) {
+            // $allCompanyIds = session()->has($this->companiesSessionName)
+            //     ? session($this->companiesSessionName)
+            //     : $request->companies_selected;
+            $allCompanyIds = $request->companies_selected;
+            $companies = $companies->whereIn('companies.id', $allCompanyIds);
+        }
 
         if ($segments) {
-            $companies = $companies->whereIn('activity_field_id', $segments);
+            $companies = $companies->whereIn('companies.activity_field_id', $segments);
         }
 
         return $companies->paginate(self::REGISTRIES_PER_PAGE);
---
+++ config/timezones.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'brazil' => 'America/Sao_Paulo'
+];
