124a21dc594dfa7cb939da642ef5ca5242f8d7e9
Renato Server Homologação <renato@acessohost.com.br>
2023-12-06 16:53:08 -0300

    corrigido

--- app/Http/Middleware/LogLoginHistory.php
+++ app/Http/Middleware/LogLoginHistory.php
@@ -6,24 +6,32 @@ use Closure;
 use App\Models\LoginHistory;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Session;
 
 class LogLoginHistory
 {
     public function handle($request, Closure $next)
     {
+        // Verifica se o usuário está autenticado
         if (Auth::check() && !$request->is('logout')) {
-            $authUser = Auth::user();
 
-            LoginHistory::create([
-                'user_id' => $authUser->id,
-                'associate_id' => (authUserIsAnAssociate()) ? $authUser->contact->company->associate->id : null,
-                'ip' => Request::ip(),
-                'user_agent' => Request::userAgent(),
-            ]);
+            // Verifica se o indicador de login recente está na sessão
+            if (!Session::has('user_just_logged_in')) {
+                $authUser = Auth::user();
+
+                LoginHistory::create([
+                    'user_id' => $authUser->id,
+                    'associate_id' => (authUserIsAnAssociate()) ? $authUser->contact->company->associate->id : null,
+                    'ip' => Request::ip(),
+                    'user_agent' => Request::userAgent(),
+                ]);
+
+                // Define o indicador de login recente na sessão
+                Session::put('user_just_logged_in', true);
+            }
         }
 
+        // Continua o fluxo normal
         return $next($request);
     }
 }
-
-
