a2024a7d500462898917cd20f7d2df299b9dd2fd
Renato Ribeiro Machado <renato@acessohost.com.br>
2024-03-13 18:21:51 -0300

    fix:alterado

--- resources/views/layouts/app_customer.blade.php
+++ resources/views/layouts/app_customer.blade.php
@@ -414,7 +414,7 @@
                 background-color: #000 !important; /* Altere para a cor desejada */
                 color: #fff !important; /* Altere para a cor desejada */
                 border: 1px solid #000 !important; /* Altera a cor da borda para preto */
-                max-width: 200px !important;
+                min-width: 50px !important;
             }
 
             /* Estilo para a borda do datepicker */
