85e12af39f369b1dbe9c994f5894d54602a14704
Renato Ribeiro Machado <renato@acessohost.com.br>
2024-03-19 18:42:08 -0300

    fix:alterado

--- app/Http/Controllers/QuarterlyResultController.php
+++ app/Http/Controllers/QuarterlyResultController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\QuarterlyResult;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Log;
 
 
 class QuarterlyResultController extends Controller
@@ -41,63 +40,30 @@ class QuarterlyResultController extends Controller
      */
 
 
-     public function store(Request $request)
+    public function store(Request $request)
     {
         // Validação dos campos
         $request->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'pdf' => 'required|mimes:pdf|max:2048',
+            //'pdf' => 'required|mimes:pdf|max:2048',
         ]);
 
-        // Cria um novo objeto QuarterlyResult
-        $quarterlyResult = new QuarterlyResult();
-
-        // Aplica o upload da imagem
-        $this->applyImageUpload($request, $quarterlyResult);
+        // Upload da imagem
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store("quarterlyResult", 'public');
+            $data['image'] = $imagePath;
+        }
 
         // Upload do PDF
         if ($request->hasFile('pdf')) {
             $pdfPath = $request->file('pdf')->store("quarterlyResult", 'public');
-            $quarterlyResult->pdf = $pdfPath;
+            $data['pdf'] = $pdfPath;
         }
 
         // Salvar os dados no banco de dados
-        try {
-            $quarterlyResult->save();
-            return redirect()->back()->with('success', 'Cadastrado com sucesso!!');
-        } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Erro ao cadastrar. Por favor, tente novamente.');
-        }
-    }
+        QuarterlyResult::create($data);
 
-
-
-    private function applyImageUpload(Request $request, QuarterlyResult $quarterlyResult): void
-    {
-        // Verifica se o arquivo de imagem está presente no request
-        if ($request->hasFile('image')) {
-            
-            // Obtém o arquivo de imagem do request
-            $file = $request->file('image');
-            
-            // Gera um nome aleatório para o arquivo
-            $fileName = uniqid() . '_' . time() . '.' . $file->getClientOriginalExtension();
-            
-            // Define o diretório de armazenamento
-            $directory = 'quarterlyResult/images';
-            
-            // Armazena a imagem no diretório especificado
-            $uploadedImage = $file->storeAs($directory, $fileName, 'public');
-            
-            // Verifica se o upload foi bem-sucedido
-            if ($uploadedImage) {
-                // Atualiza os campos do modelo com os caminhos de armazenamento
-                $quarterlyResult->storage_image_link = $directory . '/' . $fileName;
-                $quarterlyResult->public_image_link = Storage::url($directory . '/' . $fileName);
-                // Salva as alterações no banco de dados
-                $quarterlyResult->save();
-            }
-        }
+        return redirect()->back()->with('success', 'Cadastrado com sucesso!!');
     }
 
 }
