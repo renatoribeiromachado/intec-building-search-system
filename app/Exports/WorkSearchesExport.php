2c4b1663a4ee7aaf882abe68f7b9893721a7954a
Renato Ribeiro Machado <renato@acessohost.com.br>
2024-05-22 17:44:00 -0300

    fix:corrigido

--- app/Exports/WorkSearchesExport.php
+++ app/Exports/WorkSearchesExport.php
@@ -47,34 +47,72 @@ class WorkSearchesExport implements FromCollection, WithHeadings, ShouldAutoSize
     {
 
         $sql = "SELECT 
-                w.id, 
-                w.name as work, 
-                c.name, 
-                c.email,
-                c.secondary_email,
-                c.tertiary_email,
-                c.ddd,
-                c.main_phone,
-                c.ddd_two,
-                c.phone_two,
-                c.ddd_three,
-                c.phone_three,
-                c.ddd_four,
-                c.phone_four,
-                p.description as position,
-                cp.trading_name AS fantasy,
-                cp.cnpj
-            FROM works w 
-            JOIN contact_work cw ON cw.work_id = w.id
-            LEFT JOIN contacts c ON c.id = cw.contact_id
-            JOIN positions p ON p.id = c.position_id
-            JOIN companies cp ON cp.id = c.company_id
-            JOIN company_work cpw ON cpw.company_id = c.company_id AND cpw.work_id = w.id
-            WHERE w.id = $data
-            GROUP BY  w.id, c.id
-            ORDER BY w.last_review DESC, w.name ASC
-            ";
-        
+                    w.id, 
+                    w.name as work, 
+                    c.name, 
+                    c.email,
+                    c.secondary_email,
+                    c.tertiary_email,
+                    c.ddd,
+                    c.main_phone,
+                    c.ddd_two,
+                    c.phone_two,
+                    c.ddd_three,
+                    c.phone_three,
+                    c.ddd_four,
+                    c.phone_four,
+                    p.description as position,
+                    cp.trading_name AS fantasy,
+                    cp.cnpj
+                FROM 
+                    works w 
+                JOIN 
+                    (
+                        SELECT 
+                            cw.work_id,
+                            c.id,
+                            c.name, 
+                            c.email,
+                            c.secondary_email,
+                            c.tertiary_email,
+                            c.ddd,
+                            c.main_phone,
+                            c.ddd_two,
+                            c.phone_two,
+                            c.ddd_three,
+                            c.phone_three,
+                            c.ddd_four,
+                            c.phone_four,
+                            c.position_id,
+                            c.company_id, 
+                            p.description,
+                            cp.trading_name,
+                            cp.cnpj
+                        FROM 
+                            contact_work cw 
+                        LEFT JOIN 
+                            contacts c ON c.id = cw.contact_id
+                        JOIN 
+                            positions p ON p.id = c.position_id
+                        JOIN 
+                            companies cp ON cp.id = c.company_id
+                        JOIN 
+                            company_work cpw ON cpw.company_id = c.company_id AND cpw.work_id = cw.work_id
+                        WHERE 
+                            cw.work_id = $data
+                        GROUP BY 
+                            cw.work_id,
+                            c.id
+                    ) c ON w.id = c.work_id
+                LEFT JOIN 
+                    positions p ON p.id = c.position_id
+                LEFT JOIN 
+                    companies cp ON cp.id = c.company_id 
+                ORDER BY 
+                    w.last_review DESC, 
+                    w.name ASC
+             ";
+                    
         $results = \DB::select($sql);
 
         return $results;
