094b290b5ebd98796588e77841970242796691a2
Renato Ribeiro Machado <renato@acessohost.com.br>
2024-05-22 17:31:31 -0300

    fix:corrigido

--- app/Exports/WorkSearchesExport.php
+++ app/Exports/WorkSearchesExport.php
@@ -69,6 +69,7 @@ class WorkSearchesExport implements FromCollection, WithHeadings, ShouldAutoSize
             LEFT JOIN contacts c ON c.id = cw.contact_id
             JOIN positions p ON p.id = c.position_id
             JOIN companies cp ON cp.id = c.company_id
+            JOIN company_work cpw ON cpw.company_id = c.company_id AND cpw.work_id = w.id
             WHERE w.id = $data
             ORDER BY w.last_review DESC, w.name ASC
             ";
